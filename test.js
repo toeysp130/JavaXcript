65e5c07a5d1f1884863b15c8d48d2870657cce5b
toeysp130 <choptum@hotmail.co.th>
2021-01-12 15:56:15 +0700

    update test.js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-console.log("Hello World watchrk script")
+//console.log("Hello World watchrk script")
+const message = "Hello";
+const place = "World";
+
+console.log('Hello, World!');
+
+console.log('Hello, %s!',place);
+
+console.log('${message},${place}!');
