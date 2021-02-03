8cec465d23e743ec27db765e00fe94ffbf3df4b9
toeysp130 <choptum@hotmail.co.th>
2021-02-03 08:33:55 +0700

    commit

---
+++ JavaXcriptLR5/asyncawait-1.js
@@ -0,0 +1,16 @@
+function promiseTimeout(ms) {
+    return new Promise((resolve , reject) =>{
+        setTimeout(resolve,ms);
+        });
+}
+
+async function run() {
+    //logic
+    console.log("Start!!");
+    //try take of await and compare
+    await promiseTimeout(2000);
+    console.log("Stop!!");
+
+}
+
+run();
