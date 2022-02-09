f3f1e7a5178684edd9b421c0e38bd6521f3a31bb
toeysp130 <choptum@hotmail.co.th>
2022-02-09 22:58:34 +0700

    update

--- JavaXcriptLR4/libHello.js
+++ JavaXcriptLR4/libHello.js
@@ -2,15 +2,12 @@ const person ={
     name : 'Watcharak',
     weight : 85
 }
-
 function sayHello(){
     console.log("Hello world!");
 }
-
 function cube(x){
     return x*x*x;
 }
-
 const add = (a,b) => a+b;
 
 const status = true;
